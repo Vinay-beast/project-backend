470fe4af8a1edc4e9f4e1eedf584da64917e9356
Tanguturi Vinay Krishna Chetty <vinayk4607@gmail.com>
2025-09-04 00:34:29 +0530

    Use Supabase DB with SSL connection

--- config/database.js
+++ config/database.js
@@ -3,7 +3,10 @@ require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: {
+        rejectUnauthorized: false,
+        require: true
+    }
 });
 
 module.exports = pool;
