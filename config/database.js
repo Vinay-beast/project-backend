e59773d5baf68c27919c435400957d3dc58fd1d8
Tanguturi Vinay Krishna Chetty <vinayk4607@gmail.com>
2026-03-11 20:26:16 +0530

    fix: reduce connectionLimit to 3 to prevent MySQL connection overflow

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     uri: process.env.DATABASE_URL,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 3,
     queueLimit: 0
 });
 
